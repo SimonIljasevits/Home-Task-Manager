a0299e6a9c8f2d7d42762dd5a5b608474bf3a7c6
Simon Iljaševitš <simon.iljasevits@tptlive.ee>
2025-04-25 11:36:46 +0300

    Js almost done

--- js/index.js
+++ js/index.js
@@ -1,2 +1,29 @@
+$(document).ready(function() {
+    handleHomeJoining()
+  
+    });
 
+let homeName = ""
+
+function handleHomeJoining(){
+    $('#joinHomeBtn').click(function() {
+      $('#popupForm').fadeIn(200);  
+      homeName = $("#joinHomeText").val()
+      $("#formHeader").text("Join " + homeName)
+    });
+  
+
+    $('.closeFormBtn').click(function() {
+      $('#popupForm').fadeOut(200);  
+    });
+
+    $('#taskForm').submit(function(e) {
+      e.preventDefault();
+  
+      const assignee = $('#assignee').val();
+      
+      alert("You have succesfully joined " + homeName + " as " + assignee)
+      window.location.href = "home.html"
+    });
+  }
 
