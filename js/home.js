66e5953a93555a0a3ef7a10f3f753f0e18d4c2d5
Simon Iljaševitš <simon.iljasevits@tptlive.ee>
2025-05-15 23:56:59 +0300

    Project done

--- js/home.js
+++ js/home.js
@@ -1,6 +1,22 @@
+const urlParams = new URLSearchParams(window.location.search)
+const homeName = urlParams.get('home')
+const memberName = urlParams.get('memberName')
+const members = urlParams.get('members')
+const taskDb = `${homeName}-tasks`
+console.log(`Home page opened with homeName: ${homeName} and with memberName: ${memberName}`)
+console.log('Task url is: ' + taskDb)
+
 $(document).ready(function() {
   handleNewTaskCreating()
+  handleTaskClearing()
 
+  $('.megaTitle').append(` ${homeName.toUpperCase()}`)
+  fetchTasks()
+  console.log(members)
+  $('#assignee').empty()
+  members.split(',').forEach(m => {
+    $('#assignee').append(`<option value="${m}">${m}</option>`)
+  })
   });
 
   function handleNewTaskCreating(){
@@ -13,22 +29,98 @@ $(document).ready(function() {
       $('#popupForm').fadeOut(200);  
     });
 
-    $('#taskForm').submit(function(e) {
+    $('#taskForm').submit(async function(e) {
       e.preventDefault(); 
-  
+
       const taskName = $('#taskName').val();
       const assignee = $('#assignee').val();
-      
+
+      await postTask(taskName, assignee)
+
       alert(taskName + " task was assigned to " + assignee)
 
-      $('#popupForm').fadeOut(200);
+      $('#popupForm').fadeOut(200, function() {
+        window.location.reload()
+      });
       $('#taskForm')[0].reset(); 
     });
   }
 
+  async function postTask(name, assignedTo){
+    let task = {
+      'name' : name,
+      'assignedTo': assignedTo
+    }
+
+    await postDatabase(taskDb, JSON.stringify(task))
+  }
+
   function handleTaskClearing(){
-    const doneTaskBox = $('.doneTaskBox')
     $('.clearCompletedTasks').click(function(){
+      const completedTaskIds = []
+      $('.taskPlank').has('input[type="checkbox"]:checked').each(function () {
+        const id = $(this).data('id');
+        if (id !== undefined) {
+          completedTaskIds.push(id);
+        }
+      });
+
+      console.log(completedTaskIds)
       
+      $('.taskPlank').has('input[type="checkbox"]:checked').remove();
+
+      completedTaskIds.forEach(taskId => deleteDatabase(taskDb, taskId))
     })
   }
+
+function createTaskWithId(name, id){
+  return `<div class="taskPlank" data-id ="${id}">
+    <img class="taskImage" src="images/task.png" alt="Task">
+    <p style="padding-left: 15px;">${name}</p>
+    <input type="checkbox" name="isTaskDone" id="isTaskDone">
+  </div>`
+}
+
+async function fetchTasks() {
+  tasks = await fetchDatabase(taskDb)
+  tasks.forEach(t => { if (t.assignedTo === memberName){
+    $('#taskBox').append(createTaskWithId(t.name, t.id))
+  }
+  });
+}
+
+  async function postDatabase(dbName, postContent){
+    const url = "https://kool.krister.ee/chat/homeTaskManager-" + dbName;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: postContent
+    });
+    console.log(url)
+    if (!response.ok){
+      alert("Server returned " + response.status)
+    }
+  }
+
+  async function fetchDatabase(dbName) {
+    const response = await fetch("https://kool.krister.ee/chat/homeTaskManager-" + dbName);
+    const data = await response.json();
+    return data;
+}
+
+async function deleteDatabase(dbName, itemId){
+  const url = `https://kool.krister.ee/chat/homeTaskManager-${dbName}/${itemId}`;
+  const response = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: ''
+  });
+  console.log(url)
+  if (!response.ok){
+    alert("Server returned " + response.status)
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,21 +1,37 @@
 $(document).ready(function() {
     handleHomeJoining()
     handleHomeCreating()
-  
+    fetchHomeInfo('simon')
     });
 
 let homeName = ""
+let homeMembers = ['Parent']
+let homeInfoToJoin = {};
 
 function handleHomeJoining(){
-    $('#joinHomeBtn').click(function() {
-      $('#joinPopupForm').fadeIn(200);  
+    $('#joinHomeBtn').click(async function() {
       homeName = $("#joinHomeText").val()
+      if (homeName === ''){
+        console.log('You cant join home without homeName')
+        return;
+      }
+
+      const data = await fetchHomeInfo(homeName)
+      if (!data){
+        alert("Home with such name doesn't exist")
+        return;
+      }
+      homeInfoToJoin = data
+      addAssigneeOptions()
+      $('#joinPopupForm').fadeIn(200);  
       $("#joinFormHeader").text("Join " + homeName)
     });
   
 
     $('.closeJoinFormBtn').click(function() {
-      $('#joinPopupForm').fadeOut(200);  
+      $('#joinPopupForm').fadeOut(200); 
+      homeInfoToJoin = {} 
+      homeName = ""
     });
 
     $('#joinTaskForm').submit(function(e) {
@@ -24,35 +40,101 @@ function handleHomeJoining(){
       const assignee = $('#assignee').val();
       
       alert("You have succesfully joined " + homeName + " as " + assignee)
-      window.location.href = "home.html"
+      const params = new URLSearchParams({
+        home: homeName,
+        memberName: assignee,
+        members: homeInfoToJoin.members
+      });
+      window.location.href = `home.html?${params.toString()}`;
     });
   }
 
+  function addAssigneeOptions(){
+    $('#assignee').empty();
+    homeInfoToJoin.members.forEach(m => $('#assignee').append(`<option value="${m}">${m}</option>`))
+  }
+
   function handleHomeCreating(){
-    $('#createHomeBtn').click(function() {
-      $('#createPopupForm').fadeIn(200);  
+    $('#createHomeBtn').click(async function() {
       homeName = $("#createHomeText").val()
+      if (homeName === ''){
+        alert('You cant create home without homeName')
+        return;
+      }
+      const data = await fetchHomeInfo(homeName)
+      if (data){
+        alert("Home with such name already exists")
+        return;
+      }
+      $('#createPopupForm').fadeIn(200);  
       $("#createFormHeader").text(homeName + " Create Form")
     });
 
     $('#addMemberButton').click(function(){
       const memberName = $('.memberNameText').val();
       $('.memberNameText').val("")
+      homeMembers.push(memberName)
       $('.memberList').append('<p>' + memberName + '</p>');
     });
   
 
     $('.closeCreateFormBtn').click(function() {
       $('#createPopupForm').fadeOut(200);  
+      homeMembers = ['Parent']
+      homeName = ""
     });
 
-    $('#createTaskForm').submit(function(e) {
+    $('#createTaskForm').submit(async function(e) {
       e.preventDefault();
       
+      await postHomeInfo(homeName, homeMembers)
       alert("You have succesfully created home with name: " + homeName)
-      window.location.href = "home.html"
+      const params = new URLSearchParams({
+        home: homeName,
+        memberName: "Parent",
+        members: homeMembers
+      });
+      window.location.href = `home.html?${params.toString()}`;
     });
   }
 
+async function postDatabase(dbName, postContent){
+  const url = "https://kool.krister.ee/chat/homeTaskManager-" + dbName;
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: postContent
+  });
+  console.log(url)
+  if (!response.ok){
+    alert("Server returned " + response.status)
+  }
+}
+
+  async function fetchDatabase(dbName) {
+    const response = await fetch("https://kool.krister.ee/chat/homeTaskManager-" + dbName);
+    const data = await response.json();
+    return data;
+}
+
+ async function fetchHomeInfo(homeName) {
+    data = await fetchDatabase(homeName);
+    if (!data[0]){
+      console.log('HomeInfo is null') 
+      return null;
+    }
+    console.log("HomeInfo is: " + data[0].members)
+    return data[0];
+ }
+
+async function postHomeInfo(name, members){
+  let homeInfo = {}
+  homeInfo["name"] = name;
+  homeInfo["members"] = members;
+  console.log(JSON.stringify(homeInfo))
+  await postDatabase(name, JSON.stringify(homeInfo))
+}
 
  
